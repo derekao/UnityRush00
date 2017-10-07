d3c67441925b48ae83af9c5b63ccaa37fecaa981
Sebastien MARTIN <semartin@e3r7p7.42.fr>
2017-10-07 13:30:09 +0200

    al

---
+++ Script/PlayerManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	public Weapon PlayerWeapon;
+	public float fSpeed;
+
+	private Rigidbody2D RbPlayer;
+	private Animator 	AnimPlayer;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		RbPlayer = GetComponent<Rigidbody2D> ();
+		AnimPlayer = GetComponentInChildren<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		MovementInput ();
+	
+
+	}
+
+	void SetWalking(bool bWalk)
+	{
+		if (AnimPlayer.GetBool("Walk")!= bWalk)
+			AnimPlayer.SetBool("Walk", bWalk);
+	}
+
+	void SetVelocity(string sz, float speed)
+	{
+		if (sz == "x")
+		{
+			if (RbPlayer.velocity.x != speed)
+				RbPlayer.velocity = new Vector3 (speed, RbPlayer.velocity.y, 0);
+		} 
+		else if (sz == "y")
+		{
+			if (RbPlayer.velocity.y != speed)
+				RbPlayer.velocity = new Vector3 (RbPlayer.velocity.x, speed, 0);
+		}
+		else
+			Debug.Log ("Error in SetVelocity");
+	}
+
+	void MovementInput()
+	{
+		Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		diff.Normalize();
+
+		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+
+
+		if (Input.GetKey (KeyCode.W))
+		{
+			SetVelocity ("y", fSpeed);
+			SetWalking (true);
+		}
+		else if (Input.GetKey (KeyCode.S))
+		{
+			SetVelocity ("y", -fSpeed);
+			SetWalking (true);
+		}
+		else
+		{
+			SetVelocity ("y", 0);
+			SetWalking (false);
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			SetVelocity ("x", -fSpeed);
+			SetWalking (true);
+		} 
+		else if (Input.GetKey (KeyCode.D))
+		{
+			SetVelocity ("x", fSpeed);
+			SetWalking (true);
+		} 
+		else
+		{
+			SetVelocity ("x", 0);
+			SetWalking (false);
+		}
+	}
+}
---
+++ Script/RandomBody.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBody : MonoBehaviour {
+
+
+	public Sprite[] BodyTexture;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		SpriteRenderer Texture = GetComponent<SpriteRenderer>();
+		Texture.sprite = BodyTexture[Random.Range (0, BodyTexture.Length)];
+	}
+}
